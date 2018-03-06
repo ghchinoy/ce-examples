6015994f49fedd2437d75ca760798baa9079f6b0
G. Hussain Chinoy <ghchinoy@gmail.com>
2018-03-06 09:33:03 -0700

    Updates with formatting, flag for users

--- eventson/main.go
+++ eventson/main.go
@@ -2,44 +2,121 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/ghchinoy/ce-go/ce"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/viper"
 )
+
+var profileFilter string
+var showUsers bool
+
+func init() {
+	flag.StringVar(&profileFilter, "filter", "", "profile filter")
+	flag.BoolVar(&showUsers, "users", false, "show account e-mails")
+	flag.Parse()
+}
 
 func main() {
 
 	// load a configuration file
 	loadConfigFile()
-
 	// get all profiles
-	profiles := listAllProfiles()
+	allprofiles := getAllProfiles()
+	// filter profiles
+	profiles := filterList(allprofiles, profileFilter)
+	log.Printf("Querying %v/%v profiles: %s\n", len(profiles), len(allprofiles), profileFilter)
 
 	// print out the instances in the profile which have events enabled
+	data := [][]string{}
 	for _, profile := range profiles {
 		instances, err := getInstancesWithEvents(profile)
 		if err != nil {
-			log.Println(profile, err.Error())
+			log.Println(profile, "instances", err.Error())
 			//os.Exit(1)
 		}
-		if len(instances) > 0 {
-			for _, i := range instances {
 
-				fmt.Printf("%s: %v %s/%s: %s @ %s\n",
-					profile,
-					i.ID,
-					i.Element.Key,
-					i.Name,
-					i.Configuration.EventVendorType,
-					i.Configuration.EventPollerRefreshInterval,
-				)
+		if len(instances) > 0 { // this could be a goroutine returning []string
+			var userlist []string
+			// conditional for user account e-mail output
+			if showUsers {
+				users, err := getAllUsers(profile)
+				if err != nil {
+					log.Printf("%s users %s", profile, err.Error())
+				}
+
+				for _, user := range users {
+					userlist = append(userlist, user.EMail)
+				}
+			}
+			for _, i := range instances {
+				// conditional for user account e-mail output
+				if showUsers {
+					data = append(data, []string{
+						profile,
+						fmt.Sprintf("%v", userlist),
+						strconv.Itoa(i.ID),
+						i.Element.Key,
+						i.Name,
+						i.Configuration.EventVendorType,
+						i.Configuration.EventPollerRefreshInterval,
+					})
+				} else {
+					data = append(data, []string{
+						profile,
+						strconv.Itoa(i.ID),
+						i.Element.Key,
+						i.Name,
+						i.Configuration.EventVendorType,
+						i.Configuration.EventPollerRefreshInterval,
+					})
+				}
 			}
 		}
 	}
+	table := tablewriter.NewWriter(os.Stdout)
+	if showUsers {
+		table.SetHeader([]string{"Profile", "EMail", "ID", "Key", "Name", "EventType", "Interval"})
+	} else {
+		table.SetHeader([]string{"Profile", "ID", "Key", "Name", "EventType", "Interval"})
+	}
+	table.SetBorder(false)
+	table.SetAutoMergeCells(true)
+	table.AppendBulk(data)
+	table.Render()
+}
+
+func getAllUsers(profile string) ([]ce.User, error) {
+	var users []ce.User
+	if profile == "profile" {
+		return users, fmt.Errorf("'profile' is not a real profile")
+	}
+	base := viper.Get(profile + ".base").(string)
+	auth := fmt.Sprintf("User %s, Organization %s",
+		viper.Get(profile+".user").(string),
+		viper.Get(profile+".org").(string),
+	)
+	bodybytes, status, _, err := ce.GetAllUsers(base, auth)
+	if err != nil {
+		return users, err
+	}
+
+	if status != 200 {
+		return users, fmt.Errorf("Non-200 Status: %v", status)
+	}
+	err = json.Unmarshal(bodybytes, &users)
+	if err != nil {
+		return users, err
+	}
+
+	return users, nil
 }
 
 // getInstancesWithEvents returns Instances with events on for a profile
@@ -76,6 +153,7 @@ func getInstancesWithEvents(profile string) ([]ce.ElementInstance, error) {
 		return instances, err
 	}
 
+	// here's where the filtering of only event-enabled Instances occur
 	var eventInstances []ce.ElementInstance
 	for _, i := range instances {
 		if i.EventsEnabled == true {
@@ -86,8 +164,22 @@ func getInstancesWithEvents(profile string) ([]ce.ElementInstance, error) {
 	return eventInstances, nil
 }
 
-// listAllProfiles lists all the profile names within the settings
-func listAllProfiles() []string {
+// filterList returns a list containing only the filter term
+func filterList(originallist []string, filterterm string) []string {
+	var filteredlist []string
+	if filterterm == "" {
+		return originallist
+	}
+	for _, v := range originallist {
+		if strings.Contains(v, filterterm) {
+			filteredlist = append(filteredlist, v)
+		}
+	}
+	return filteredlist
+}
+
+// getAllProfiles lists all the profile names within the settings
+func getAllProfiles() []string {
 	settings := viper.AllSettings()
 	var profiles []string
 	for k := range settings {
